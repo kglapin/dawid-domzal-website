c3459b1017ba6c5816bd441bb0094de88aee10d9
kglapin <kamilglapinski.poczta@gmail.com>
2026-07-20 11:09:18 +0200

    Assemble optimized hero image

---
+++ src/data/heroImage.ts
@@ -0,0 +1,6 @@
+import part01 from "./hero/part01";
+import part02 from "./hero/part02";
+import part03 from "./hero/part03";
+import part04 from "./hero/part04";
+
+export const heroImage = `data:image/webp;base64,${part01}${part02}${part03}${part04}`;
