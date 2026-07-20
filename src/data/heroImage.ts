e8fe032afe65b1184a63bedf055ddd9ca8a5db7b
kglapin <kamilglapinski.poczta@gmail.com>
2026-07-20 11:12:07 +0200

    Prepare optimized hero image for static route

--- src/data/heroImage.ts
+++ src/data/heroImage.ts
@@ -3,4 +3,4 @@ import part02 from "./hero/part02";
 import part03 from "./hero/part03";
 import part04 from "./hero/part04";
 
-export const heroImage = `data:image/webp;base64,${part01}${part02}${part03}${part04}`;
+export const heroImageBase64 = `${part01}${part02}${part03}${part04}`;
